a734e0c0e15be04cd9a4671a3171dec3953f64da
Nir Friedman <quicknir@gmail.com>
2022-08-14 19:02:53 -0400

    First version of code; just nodes; no graph

--- comp_graph/src/lib.rs
+++ comp_graph/src/lib.rs
@@ -1,3 +1,5 @@
+#![feature(strict_provenance)]
+
 pub mod foo {
 pub fn hello() {
     println!("Hello, world!");
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,66 @@
-extern crate comp_graph;
-use comp_graph::foo::hello;
+use std::ptr;
+
+pub trait ComputationalNode {
+    fn evaluate(&mut self);
+}
+
+struct Output<T> {
+    data: T,
+}
+
+impl<T: Default> Default for Output<T> {
+    fn default() -> Output<T> {
+        Output::<T> {
+            data: Default::default(),
+        }
+    }
+}
+
+struct Input<T> {
+    data: *const T,
+}
+
+impl<T> Input<T> {
+    fn get(&self) -> &T {
+        unsafe { &*self.data }
+    }
+}
+
+impl<T> Default for Input<T> {
+    fn default() -> Input<T> {
+        Input::<T> { data: ptr::null() }
+    }
+}
+
+struct Node1 {
+    output1: Output<f64>,
+}
+
+impl ComputationalNode for Node1 {
+    fn evaluate(&mut self) {
+        self.output1.data += 1.0
+    }
+}
+
+struct Node2 {
+    input1: Input<f64>,
+}
+
+impl ComputationalNode for Node2 {
+    fn evaluate(&mut self) {
+        println!("{}", self.input1.get())
+    }
+}
+
 fn main() {
-    hello();
-    println!("Hello, world!");
+    let mut first = Node1 {
+        output1: Default::default(),
+    };
+    let mut second = Node2 {
+        input1: Default::default(),
+    };
+    second.input1.data = &first.output1.data;
+    first.evaluate();
+    first.evaluate();
+    second.evaluate();
 }
