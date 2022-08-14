1051504dadb3b35cabed6adb409b25f9280f8afe
Nir Friedman <quicknir@gmail.com>
2022-08-14 19:06:31 -0400

    Add type erasure to nodes

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::ptr;
+use std::ops::{Deref, DerefMut};
+
 
 pub trait ComputationalNode {
-    fn evaluate(&mut self);
+    fn evaluate(& mut self);
 }
 
 struct Output<T> {
@@ -10,19 +12,20 @@ struct Output<T> {
 
 impl<T: Default> Default for Output<T> {
     fn default() -> Output<T> {
-        Output::<T> {
-            data: Default::default(),
-        }
+        Output::<T> { data: Default::default() }
     }
 }
 
+
 struct Input<T> {
-    data: *const T,
+    data: * const T,
 }
 
 impl<T> Input<T> {
     fn get(&self) -> &T {
-        unsafe { &*self.data }
+        unsafe {
+            &*self.data
+        }
     }
 }
 
@@ -36,6 +39,13 @@ struct Node1 {
     output1: Output<f64>,
 }
 
+impl Node1 {
+    fn new() -> Box<Self> {
+        Box::new(Node1{output1: Default::default()})
+    }
+}
+
+
 impl ComputationalNode for Node1 {
     fn evaluate(&mut self) {
         self.output1.data += 1.0
@@ -46,21 +56,28 @@ struct Node2 {
     input1: Input<f64>,
 }
 
+impl Node2 {
+    fn new() -> Box<Self> {
+        Box::new(Node2{input1: Default::default()})
+    }
+}
+
 impl ComputationalNode for Node2 {
     fn evaluate(&mut self) {
         println!("{}", self.input1.get())
     }
 }
 
+
 fn main() {
-    let mut first = Node1 {
-        output1: Default::default(),
-    };
-    let mut second = Node2 {
-        input1: Default::default(),
-    };
+    let mut v = Vec::<Box<dyn ComputationalNode>>::new();
+    let mut first = Node1::new();
+    let mut second = Node2::new();
     second.input1.data = &first.output1.data;
-    first.evaluate();
-    first.evaluate();
-    second.evaluate();
+    v.push(first);
+    v.push(second);
+    v[0].evaluate();
+    v[1].evaluate();
+    v[0].evaluate();
+    v[1].evaluate();
 }
