7476633ee404af2362056454732d36f6cd8502b6
Nir Friedman <quicknir@gmail.com>
2022-08-14 20:32:26 -0400

    Prepare API for eliminating UB/provenance issues

--- comp_graph/src/lib.rs
+++ comp_graph/src/lib.rs
@@ -2,171 +2,234 @@
 
 pub mod compute_graph {
 
-use std::collections::HashMap;
-use std::any::Any;
-use std::marker::PhantomData;
-use core::cell::UnsafeCell;
-use std::ptr;
+    use core::cell::UnsafeCell;
+    use std::any::Any;
+    use std::collections::HashMap;
+    use std::marker::PhantomData;
+    use std::ptr;
 
-
-pub trait ComputationalNode {
-    type Attributes;
-    type DeclareInfo;
-    fn evaluate(&mut self, atts: &Self::Attributes);
-    fn declare_attributes(atts: &Self::Attributes, declare_info: Self::DeclareInfo) -> NodeAttributes;
-}
-
-trait ErasedNode {
-    fn evaluate(&mut self);
-    fn declare_attributes(&self, declare_info: Box<dyn Any>) -> NodeAttributes;
-}
-
-struct ErasedNodeImpl<T: ComputationalNode + 'static> {
-    node: T,
-    atts: T::Attributes,
-}
-
-impl<T: ComputationalNode + 'static> ErasedNode for ErasedNodeImpl<T> {
-    fn evaluate(&mut self) { self.node.evaluate(&self.atts); }
-    fn declare_attributes(&self, declare_info: Box<dyn Any>) -> NodeAttributes  { 
-        T::declare_attributes(& self.atts, *declare_info.downcast::<T::DeclareInfo>().unwrap())
+    #[derive(Default)]
+    pub struct Output<T> {
+        data: UnsafeCell<T>,
     }
-}
 
-pub struct Output<T> {
-    data: UnsafeCell<T>,
-}
-
-impl<T: Default> Default for Output<T> {
-    fn default() -> Output<T> {
-        Output::<T> { data: Default::default() }
+    impl<T> Output<T> {
+        pub fn get(&self) -> &T {
+            unsafe { &*self.data.get() }
+        }
+        pub fn get_mut(&mut self) -> &mut T {
+            unsafe { &mut *self.data.get() }
+        }
     }
-}
 
-impl<T> Output<T> {
-    pub fn get(&self) -> &T { unsafe { &*self.data.get() } } 
-    pub fn get_mut(&self) -> &mut T { unsafe { &mut *self.data.get() } } 
-}
-
-
-pub struct Input<T> {
-    data: UnsafeCell<* const UnsafeCell<T>>,
-}
-
-impl <T> Input<T> {
-    pub fn get(&self) -> &T { unsafe { &*(**self.data.get()).get() } } 
-}
-
-impl<T> Default for Input<T> {
-    fn default() -> Input<T> {
-        Input::<T> { data: UnsafeCell::new(ptr::null()) }
+    #[derive(Copy, Clone)]
+    pub struct InputMaker {
+        phantom: PhantomData<i32>,
     }
-}
 
-trait InputSetter {
-    fn set(&mut self, target: & Box<dyn Any>);
-}
+    pub struct Input<T> {
+        data: UnsafeCell<*const UnsafeCell<T>>,
+    }
 
-impl<T: 'static> InputSetter for * const Input<T> {
-    fn set(&mut self, target: & Box<dyn Any>) {
-        match target.downcast_ref::<* const UnsafeCell<T>>() {
-            None => { assert!(false, "Input type and output type mismatch!") }
-            Some(t) => {
-                unsafe {
+    impl<T> Input<T> {
+        pub fn get(&self) -> &T {
+            unsafe { &*(**self.data.get()).get() }
+        }
+    }
+
+    impl<T> Input<T> {
+        pub fn new(_: InputMaker) -> Input<T> {
+            Input::<T> {
+                data: UnsafeCell::new(ptr::null()),
+            }
+        }
+    }
+
+    pub unsafe trait UnsafeNode {
+        fn evaluate(&mut self);
+        fn declare_attributes(&mut self, declare_info: Box<dyn Any>) -> NodeAttributes;
+    }
+
+    // These traits to be implemented automatically and safely by Derive macros
+    pub unsafe trait OutputStruct {
+        fn declare_outputs(&self) -> OutputAttributes;
+    }
+
+    pub unsafe trait InputStruct {
+        fn new(_: InputMaker) -> Self;
+    }
+
+    // Safe trait, most users just implement this
+    pub trait ComputationalNode {
+        type Outputs: OutputStruct;
+        type Inputs: InputStruct;
+        type InitInfo;
+
+        fn create_outputs(&mut self, init_info: &Self::InitInfo) -> Self::Outputs;
+
+        fn initialize<'a>(
+            &mut self,
+            init_info: Self::InitInfo,
+            inputs: &'a Self::Inputs,
+        ) -> InputAttributes<'a>;
+
+        fn evaluate(&mut self, inputs: &Self::Inputs, outputs: &mut Self::Outputs);
+    }
+
+    pub struct ErasedNode<T: ComputationalNode + 'static> {
+        node: T,
+        inputs: T::Inputs,
+        outputs: T::Outputs,
+    }
+
+    unsafe impl<T: ComputationalNode + 'static> UnsafeNode for ErasedNode<T> {
+        fn evaluate(&mut self) {
+            self.node.evaluate(&self.inputs, &mut self.outputs);
+        }
+        fn declare_attributes(&mut self, declare_info: Box<dyn Any>) -> NodeAttributes {
+            let mut output_attrs = self.outputs.declare_outputs();
+            let input_attrs = self.node.initialize(
+                *declare_info.downcast::<T::InitInfo>().unwrap(),
+                &self.inputs,
+            );
+            NodeAttributes {
+                inputs: input_attrs,
+                outputs: output_attrs,
+            }
+        }
+    }
+
+    trait InputSetter {
+        fn set(&mut self, target: &Box<dyn Any>);
+    }
+
+    impl<T: 'static> InputSetter for *const Input<T> {
+        fn set(&mut self, target: &Box<dyn Any>) {
+            match target.downcast_ref::<*const UnsafeCell<T>>() {
+                None => {
+                    assert!(false, "Input type and output type mismatch!")
+                }
+                Some(t) => unsafe {
                     *(**self).data.get() = *t;
+                },
+            }
+        }
+    }
+
+    pub struct OutputAttributes<'a> {
+        data: HashMap<String, Box<dyn Any>>,
+        phantom: PhantomData<&'a dyn Any>,
+    }
+
+    impl<'a> OutputAttributes<'a> {
+        pub fn new() -> OutputAttributes<'static> {
+            OutputAttributes {
+                data: HashMap::new(),
+                phantom: PhantomData,
+            }
+        }
+
+        pub fn add<T: 'static>(&mut self, name: String, output: &'a Output<T>) {
+            self.data
+                .insert(name, Box::new(&output.data as *const UnsafeCell<T>));
+        }
+    }
+
+    pub struct InputAttributes<'a> {
+        data: HashMap<String, Box<dyn InputSetter>>,
+        phantom: PhantomData<&'a dyn Any>,
+    }
+
+    impl<'a> InputAttributes<'a> {
+        pub fn new() -> InputAttributes<'static> {
+            InputAttributes {
+                data: HashMap::new(),
+                phantom: PhantomData,
+            }
+        }
+        pub fn add<T: 'static>(&mut self, name: String, input: &'a Input<T>) {
+            self.data.insert(name, Box::new(input as *const Input<T>));
+        }
+    }
+
+    pub struct NodeAttributes<'a> {
+        pub inputs: InputAttributes<'a>,
+        pub outputs: OutputAttributes<'a>,
+    }
+
+    pub struct DeclaredNode {
+        node: Box<dyn UnsafeNode + 'static>,
+        init_info: Box<dyn Any>,
+    }
+
+    impl DeclaredNode {
+        pub fn new<T: ComputationalNode + 'static>(
+            mut node: T,
+            mut init_info: T::InitInfo,
+        ) -> DeclaredNode {
+            let inputs = T::Inputs::new(InputMaker {
+                phantom: PhantomData,
+            });
+            let outputs = node.create_outputs(&init_info);
+            let node = Box::new(ErasedNode {
+                node,
+                inputs,
+                outputs,
+            });
+            let init_info = Box::new(init_info);
+            DeclaredNode { node, init_info }
+        }
+    }
+
+    pub struct GraphBuilder {
+        nodes: Vec<Box<dyn UnsafeNode>>,
+        inputs: HashMap<String, Vec<Box<dyn InputSetter>>>,
+        outputs: HashMap<String, Box<dyn Any>>,
+    }
+
+    pub struct Graph {
+        nodes: Vec<Box<dyn UnsafeNode>>,
+    }
+
+    impl GraphBuilder {
+        pub fn new() -> GraphBuilder {
+            GraphBuilder {
+                nodes: Vec::new(),
+                inputs: HashMap::new(),
+                outputs: HashMap::new(),
+            }
+        }
+        pub fn add(&mut self, name: &str, declared_node: DeclaredNode) {
+            let mut node = declared_node.node;
+            let attributes = node.declare_attributes(declared_node.init_info);
+
+            for (key, value) in attributes.inputs.data {
+                if !self.inputs.contains_key(&key) {
+                    self.inputs.insert(key.clone(), Vec::new());
+                }
+                self.inputs.get_mut(&key).unwrap().push(value);
+            }
+            for (key, value) in attributes.outputs.data {
+                self.outputs.insert(format!("{name}.{key}"), value);
+            }
+
+            self.nodes.push(node);
+        }
+        pub fn build(mut self) -> Graph {
+            for (key, value) in &mut self.inputs {
+                for input_setter in value {
+                    input_setter.set(self.outputs.get(key).unwrap());
                 }
             }
+            Graph { nodes: self.nodes }
         }
     }
-}
 
-pub struct NodeAttributes<'a> {
-    inputs: HashMap<String, Box<dyn InputSetter>>,
-    outputs: HashMap<String, Box<dyn Any>>,
-    phantom: PhantomData<&'a dyn Any>,
-}
-
-impl <'a> NodeAttributes<'a> {
-    pub fn new() -> NodeAttributes<'static> {
-        NodeAttributes{inputs: HashMap::new(), outputs: HashMap::new(), phantom: PhantomData }
-    }
-
-    pub fn add_input<T: 'static>(&mut self, name: String, input: &'a Input<T>) {
-        self.inputs.insert(name, Box::new(input as * const Input<T>));
-    }
-
-    pub fn add_output<T: 'static>(&mut self, name: String, output: &'a Output<T>) {
-        self.outputs.insert(name, Box::new(&output.data as * const UnsafeCell<T>));
-    }
-}
-
-
-pub struct DeclaredNode {
-    node: Box<dyn ErasedNode + 'static>,
-    declared_info: Box<dyn Any>,
-}
-
-impl DeclaredNode {
-    pub fn new<T: ComputationalNode + 'static>(node: T, atts: T::Attributes, static_declared_info: T::DeclareInfo) -> DeclaredNode {
-        let node = Box::new(ErasedNodeImpl{node, atts});
-        let declared_info = Box::new(static_declared_info);
-        DeclaredNode{node, declared_info}
-    }
-}
-
-
-pub struct GraphBuilder {
-    nodes: Vec<Box<dyn ErasedNode>>,
-    inputs: HashMap<String, Vec::<Box<dyn InputSetter>>>,
-    outputs: HashMap<String, Box<dyn Any>>,
-}
-
-pub struct Graph {
-    nodes: Vec<Box<dyn ErasedNode>>,    
-}
-
-
-impl GraphBuilder {
-   pub fn new() -> GraphBuilder {
-       GraphBuilder{nodes: Vec::new(), inputs: HashMap::new(), outputs: HashMap::new() }
-   }
-   pub fn add(&mut self, name: &str, declared_node: DeclaredNode) {
-        let mut node = declared_node.node;
-        let attributes = node.declare_attributes(declared_node.declared_info);
-
-        for (key, value) in attributes.inputs {
-            if !self.inputs.contains_key(&key) {
-                self.inputs.insert(key.clone(), Vec::new());
+    impl Graph {
+        pub fn evaluate(&mut self) {
+            for n in &mut self.nodes {
+                n.evaluate();
             }
-            self.inputs.get_mut(&key).unwrap().push(value);
-        }
-        for (key, value) in attributes.outputs {
-            self.outputs.insert(format!("{name}.{key}"), value);
-        }
-        
-        self.nodes.push(node);
-   }
-   pub fn build(mut self) -> Graph {
-       for (key, value) in &mut self.inputs {
-           for input_setter in value {
-               input_setter.set(self.outputs.get(key).unwrap());
-           }
-       }
-       Graph{nodes: self.nodes}
-   }
-}
-
-impl Graph {
-    pub fn evaluate(&mut self) {
-        for n in &mut self.nodes {
-             n.evaluate();
         }
     }
 }
-
-}
-
-
-
-
-
--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,84 @@
 extern crate comp_graph;
-use comp_graph::compute_graph::{Output, Input, ComputationalNode, DeclaredNode, Graph, GraphBuilder, NodeAttributes};
+use comp_graph::compute_graph::{Output, Input, ComputationalNode, DeclaredNode, Graph, GraphBuilder, NodeAttributes, InputStruct, OutputStruct, InputAttributes, OutputAttributes, InputMaker};
 
 use std::marker::PhantomData;
 
 #[derive(Default)]
-struct Node1Attributes {
+struct Node1Outputs {
     output1: Output<f64>,
     output2: Output<f64>,
 }
 
-struct Node1DeclareInfo;
+unsafe impl OutputStruct for Node1Outputs {
+    fn declare_outputs(&self) -> OutputAttributes {
+        let mut outputs = OutputAttributes::new();
+        outputs.add("x".to_string(), &self.output1);
+        outputs.add("y".to_string(), &self.output2);
+        outputs
+    }
+}
+
+struct Node1Inputs;
+
+unsafe impl InputStruct for Node1Inputs {
+    fn new(_: InputMaker) -> Self {
+        Node1Inputs {}
+    }
+}
+
+struct Node1InitInfo;
 
 struct Node1;
 
 impl ComputationalNode for Node1 {
-    type Attributes = Node1Attributes;
-    type DeclareInfo = Node1DeclareInfo;
+    type Outputs = Node1Outputs;
+    type Inputs = Node1Inputs;
+    type InitInfo = Node1InitInfo;
 
-    fn evaluate(&mut self, atts: &Self::Attributes) {
-        *atts.output1.get_mut() += 1.0;
-        *atts.output2.get_mut() += 2.0;
+    fn create_outputs(&mut self, init_info: &Self::InitInfo) -> Self::Outputs {
+        Default::default()
     }
 
-    fn declare_attributes(atts: &Self::Attributes, declare_info: Self::DeclareInfo) -> NodeAttributes {
-        let mut attributes = NodeAttributes::new();
-   
-        attributes.add_output("x".to_string(), &atts.output1);
-        attributes.add_output("y".to_string(), &atts.output2);
+    fn initialize<'a>(
+        &mut self,
+        init_info: Self::InitInfo,
+        inputs: &'a Self::Inputs,
+    ) -> InputAttributes<'a> {
+        InputAttributes::new()
+    }
 
-        attributes
+    fn evaluate(&mut self, inputs: &Self::Inputs, outputs: &mut Self::Outputs) {
+        *outputs.output1.get_mut() += 1.0;
+        *outputs.output2.get_mut() += 2.0;
     }
 }
 
 fn make_node1() -> DeclaredNode {
-    DeclaredNode::new(Node1{}, Default::default(), Node1DeclareInfo{})
+    DeclaredNode::new(Node1 {}, Node1InitInfo {})
 }
 
+struct PrinterOutputs;
 
-struct PrinterDeclareInfo {
+unsafe impl OutputStruct for PrinterOutputs {
+    fn declare_outputs(&self) -> OutputAttributes {
+        OutputAttributes::new()
+    }
+}
+
+struct PrinterInitInfo {
     input_name: String,
 }
 
-struct PrinterAttributes<T> {
+struct PrinterInputs<T> {
     input: Input<T>,
+}
+
+unsafe impl<T> InputStruct for PrinterInputs<T> {
+    fn new(i: InputMaker) -> Self {
+        PrinterInputs {
+            input: Input::new(i),
+        }
+    }
 }
 
 struct Printer<T: std::fmt::Display> {
@@ -50,72 +86,135 @@ struct Printer<T: std::fmt::Display> {
     phantom: PhantomData<T>,
 }
 
+impl<T: std::fmt::Display + 'static> ComputationalNode for Printer<T> {
+    type Inputs = PrinterInputs<T>;
+    type Outputs = PrinterOutputs;
+    type InitInfo = PrinterInitInfo;
 
-impl <T: std::fmt::Display + 'static> ComputationalNode for Printer<T> {
-    type Attributes = PrinterAttributes<T>;
-    type DeclareInfo = PrinterDeclareInfo;
-
-    fn evaluate(&mut self, atts: &Self::Attributes) {
-        println!("Printing: {}, output: {}", self.print_prefix, *atts.input.get());
+    fn initialize<'a>(
+        &mut self,
+        init_info: Self::InitInfo,
+        inputs: &'a Self::Inputs,
+    ) -> InputAttributes<'a> {
+        let mut input_atts = InputAttributes::new();
+        input_atts.add(init_info.input_name, &inputs.input);
+        input_atts
     }
 
-    fn declare_attributes(atts: &Self::Attributes, declare_info: Self::DeclareInfo) -> NodeAttributes {
-        let mut attributes = NodeAttributes::new();
+    fn evaluate(&mut self, inputs: &Self::Inputs, outputs: &mut Self::Outputs) {
+        println!(
+            "Printing: {}, input: {}",
+            self.print_prefix,
+            *inputs.input.get()
+        );
+    }
 
-        attributes.add_input(declare_info.input_name, &atts.input);
-        attributes
+    fn create_outputs(&mut self, init_info: &Self::InitInfo) -> Self::Outputs {
+        PrinterOutputs {}
     }
 }
 
-fn make_printer<T: std::fmt::Display + 'static>(input_name: String, print_prefix: String) -> DeclaredNode {
-    DeclaredNode::new(Printer::<T>{print_prefix, phantom: PhantomData}, PrinterAttributes::<T>{input: Default::default()}, PrinterDeclareInfo{input_name})
+fn make_printer<T: std::fmt::Display + 'static>(
+    input_name: String,
+    print_prefix: String,
+) -> DeclaredNode {
+    DeclaredNode::new(
+        Printer::<T> {
+            print_prefix,
+            phantom: PhantomData,
+        },
+        PrinterInitInfo { input_name },
+    )
 }
 
-struct MultiplierDeclareInfo {
+struct MultiplierInitInfo {
     input1_name: String,
     input2_name: String,
 }
 
 #[derive(Default)]
-struct MultiplierAttributes {
+struct MultiplierOutputs {
+    product: Output<f64>,
+}
+
+unsafe impl OutputStruct for MultiplierOutputs {
+    fn declare_outputs(&self) -> OutputAttributes {
+        let mut out_atts = OutputAttributes::new();
+        out_atts.add("product".to_string(), &self.product);
+        out_atts
+    }
+}
+
+struct MultiplierInputs {
     input1: Input<f64>,
     input2: Input<f64>,
-    product: Output<f64>,
+}
+
+unsafe impl InputStruct for MultiplierInputs {
+    fn new(i: InputMaker) -> Self {
+        MultiplierInputs {
+            input1: Input::new(i),
+            input2: Input::new(i),
+        }
+    }
 }
 
 struct Multiplier;
 
 impl ComputationalNode for Multiplier {
-    type Attributes = MultiplierAttributes;
-    type DeclareInfo = MultiplierDeclareInfo;
+    type InitInfo = MultiplierInitInfo;
+    type Inputs = MultiplierInputs;
+    type Outputs = MultiplierOutputs;
 
-    fn evaluate(&mut self, atts: &Self::Attributes) {
-        *atts.product.get_mut() = *atts.input1.get() * (*atts.input2.get());
+    fn initialize<'a>(
+        &mut self,
+        init_info: Self::InitInfo,
+        inputs: &'a Self::Inputs,
+    ) -> InputAttributes<'a> {
+        let mut input_atts = InputAttributes::new();
+        input_atts.add(init_info.input1_name, &inputs.input1);
+        input_atts.add(init_info.input2_name, &inputs.input2);
+        input_atts
     }
 
-    fn declare_attributes(atts: &Self::Attributes, declare_info: Self::DeclareInfo) -> NodeAttributes {
-        let mut attributes = NodeAttributes::new();
-        attributes.add_input(declare_info.input1_name, &atts.input1);
-        attributes.add_input(declare_info.input2_name, &atts.input2);
-        attributes.add_output("product".to_string(), &atts.product);
-        attributes
+    fn evaluate(&mut self, inputs: &Self::Inputs, outputs: &mut Self::Outputs) {
+        *outputs.product.get_mut() = *inputs.input1.get() * (*inputs.input2.get());
+    }
+
+    fn create_outputs(&mut self, init_info: &Self::InitInfo) -> Self::Outputs {
+        Default::default()
     }
 }
-
 
 fn make_multiplier(input1_name: String, input2_name: String) -> DeclaredNode {
-    DeclaredNode::new(Multiplier{}, Default::default(), MultiplierDeclareInfo{input1_name, input2_name})
+    DeclaredNode::new(
+        Multiplier {},
+        MultiplierInitInfo {
+            input1_name,
+            input2_name,
+        },
+    )
 }
-
-
 
 fn main() {
     let mut builder = GraphBuilder::new();
     builder.add("start", make_node1());
-    builder.add("print_x", make_printer::<f64>("start.x".to_string(), "x".to_string()));
-    builder.add("print_y", make_printer::<f64>("start.y".to_string(), "y".to_string()));
-    builder.add("product", make_multiplier("start.x".to_string(), "start.y".to_string()));
-    builder.add("print_product", make_printer::<f64>("product.product".to_string(), "product".to_string()));
+    builder.add(
+        "print_x",
+        make_printer::<f64>("start.x".to_string(), "x".to_string()),
+    );
+    builder.add(
+        "print_y",
+        make_printer::<f64>("start.y".to_string(), "y".to_string()),
+    );
+    builder.add(
+        "product",
+        make_multiplier("start.x".to_string(), "start.y".to_string()),
+    );
+    builder.add(
+        "print_product",
+        make_printer::<f64>("product.product".to_string(), "product".to_string()),
+    );
 
     let mut graph = builder.build();
     graph.evaluate();
